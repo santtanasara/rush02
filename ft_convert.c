e234400967d2d0f1109ab06ea9f398d684f4e3f7
Sara Santana <50959582+santtanasara@users.noreply.github.com>
2025-03-30 19:34:48 -0300

    Update ft_convert.c

--- ft_convert.c
+++ ft_convert.c
@@ -18,9 +18,12 @@ unsigned long ft_atoi(const char *str)
 // Função principal que converte e imprime números compostos
 void print_number(t_dict *dict, unsigned long num)
 {
+    // Casos básicos (0-20, dezenas redondas, 100, 1000, magnitudes maiores)
     if (num <= 20 || num == 30 || num == 40 || num == 50 || 
         num == 60 || num == 70 || num == 80 || num == 90 || 
-        num == 100 || num >= 1000)
+        num == 100 || num == 1000 || num == 1000000 || num == 1000000000 ||
+        num == 1000000000000 || num == 1000000000000000 || num == 1000000000000000000
+       )
     {
         char *word = get_word(dict, num);
         if (!word) dict_error(dict);
@@ -31,40 +34,55 @@ void print_number(t_dict *dict, unsigned long num)
     // Números 21-99
     if (num < 100)
     {
-        print_number(dict, (num / 10) * 10); // Dezena (40)
+        print_number(dict, (num / 10) * 10);
         write(1, "-", 1);
-        print_number(dict, num % 10);       // Unidade (2)
+        print_number(dict, num % 10);
         return;
     }
     
     // Centenas (101-999)
     if (num < 1000)
     {
-        print_number(dict, num / 100);      // Centena (1)
+        print_number(dict, num / 100);
         write(1, " ", 1);
-        print_number(dict, 100);            // "hundred"
+        print_number(dict, 100);
         if (num % 100 != 0)
         {
             write(1, " and ", 5);
-            print_number(dict, num % 100);  // Resto (23)
+            print_number(dict, num % 100);
         }
         return;
     }
-    
 
-        // Centenas (1000-100000)
-    if (num < 1000000)
-    {
-        print_number(dict, num / 1000);      // Centena (1)
-        write(1, " ", 1);
-        print_number(dict, 1000);            // "hundred"
-        if (num % 1000 != 0)
-        {
-            write(1, " and ", 6);
-            print_number(dict, num % 1000);  // Resto (23)
+    // Array de magnitudes (mil, milhão, bilhão, etc.)
+    static const struct {
+        unsigned long magnitude;
+        const char *name;
+    } magnitudes[] = {
+        {1000000000000000000, "sextillion"},
+        {1000000000000000, "quintillion"},
+        {1000000000000, "quadrillion"},
+        {1000000000, "billion"},
+        {1000000, "million"},
+        {1000, "thousand"},
+        {0, NULL}
+    };
+
+    // Processa números grandes (>= 1000)
+    for (int i = 0; magnitudes[i].magnitude != 0; i++) {
+        if (num >= magnitudes[i].magnitude) {
+            unsigned long big_part = num / magnitudes[i].magnitude;
+            unsigned long small_part = num % magnitudes[i].magnitude;
+            
+            print_number(dict, big_part);
+            write(1, " ", 1);
+            write(1, magnitudes[i].name, strlen(magnitudes[i].name));
+            
+            if (small_part != 0) {
+                write(1, " ", 1);
+                print_number(dict, small_part);
+            }
+            return;
         }
-        return;
     }
-    // Números maiores (milhares, milhões...)
-    // ... (implementação similar para magnitudes maiores)
 }
